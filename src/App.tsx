41c244ed39443ccd60b677e20929df96c9322adf
tofu4956 <tofu4956@users.noreply.github.com>
2021-08-03 05:01:30 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -18,6 +19,12 @@ function App() {
         >
           Foooooooooooooo!!!!!!
         </a>
+        <Button
+          href="./App.tsx" 
+          variant="primary"
+        >
+          Hello?
+        </Button>{}
       </header>
     </div>
   );

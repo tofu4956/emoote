b31479b4fe6359ba660ed196253cc3e0f9d2c953
tofu4956 <tofu4956@users.noreply.github.com>
2021-08-03 03:28:45 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Foooooooooooooo!!!!!
+          Foooooooooooooo!!!!!!
         </a>
       </header>
     </div>
